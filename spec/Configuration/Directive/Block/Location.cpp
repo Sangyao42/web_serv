bb12ce15ea1744d617d4ba0661f3ffc518ab5a91
Anthony Tsang <anthonytsang0226@gmail.com>
2024-03-31 23:53:36 +0200

    spec: Add test for the best_match method on the Location directive

--- spec/Configuration/Directive/Block/Location.cpp
+++ spec/Configuration/Directive/Block/Location.cpp
@@ -29,3 +29,64 @@ TEST(TestDirectiveLocation, constructor_copy)
   ASSERT_EQ(directive2.context(), Directive::Context(50));
   ASSERT_EQ(directive2.index(), 50);
 }
+
+TEST(TestDirectiveLocation, best_match_use_first_encountered_location)
+{
+  directive::LocationBlock directive;
+  
+  directive.set("/");
+
+  {
+    directive::LocationBlock*  directive1 = new directive::LocationBlock();
+    directive1->set("/");
+    directive.add_directive(directive1);
+
+    ASSERT_EQ(directive.best_match("/"), &directive);
+  }
+}
+
+TEST(TestDirectiveLocation, best_match_use_first_encountered_location2)
+{
+  directive::LocationBlock directive;
+  
+  directive.set("/");
+
+  {
+    directive::LocationBlock*  directive1 = new directive::LocationBlock();
+    directive1->set("/image");
+    directive.add_directive(directive1);
+
+    directive::LocationBlock*  directive2 = new directive::LocationBlock();
+    directive2->set("/image");
+    directive.add_directive(directive2);
+
+    ASSERT_EQ(directive.best_match("/image/hi.html"), directive1);
+  }
+}
+
+TEST(TestDirectiveLocation, best_match_use_longest_match)
+{
+  directive::LocationBlock directive;
+  
+  directive.set("/");
+
+  {
+    directive::LocationBlock*  directive1 = new directive::LocationBlock();
+    directive1->set("/first");
+    directive.add_directive(directive1);
+
+    directive::LocationBlock*  directive2 = new directive::LocationBlock();
+    directive2->set("/first/second");
+    directive.add_directive(directive2);
+
+    directive::LocationBlock*  directive3 = new directive::LocationBlock();
+    directive3->set("/first/second/third");
+    directive.add_directive(directive3);
+
+    directive::LocationBlock*  directive4 = new directive::LocationBlock();
+    directive4->set("/first/second/third/forth");
+    directive.add_directive(directive4);
+
+    ASSERT_EQ(directive.best_match("/first/second/third/forth/aye.html"), directive4);
+  }
+}
