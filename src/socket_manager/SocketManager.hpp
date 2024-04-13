61698017b0fc168390a1998aa292e3bde9326aa9
Sangyao42 <wangsangyao@hotmail.com>
2024-04-13 16:12:55 +0200

    doc(SocketManagerClass): Add doc for socket address structure

--- src/socket_manager/SocketManager.hpp
+++ src/socket_manager/SocketManager.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "SocketError.hpp"
-#include "../src/Configuration.hpp"
+#include "../Configuration.hpp"
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,76 +10,6 @@
 
 #include <vector>
 #include <iostream>
-
-/**
- *
-// All pointers to socket address structures are often cast to pointers
-// to this type before use in various functions and system calls:
-
-struct sockaddr {
-    unsigned short    sa_family;    // address family, AF_xxx
-    char              sa_data[14];  // 14 bytes of protocol address
-};
-
-
-// IPv4 AF_INET sockets:
-
-struct sockaddr_in {
-    short            sin_family;   // e.g. AF_INET, AF_INET6
-    unsigned short   sin_port;     // e.g. htons(3490)
-    struct in_addr   sin_addr;     // see struct in_addr, below
-    char             sin_zero[8];  // zero this if you want to
-};
-
-struct in_addr {
-    unsigned long s_addr;          // load with inet_pton()
-};
-
-
-// IPv6 AF_INET6 sockets:
-
-struct sockaddr_in6 {
-    u_int16_t       sin6_family;   // address family, AF_INET6
-    u_int16_t       sin6_port;     // port number, Network Byte Order
-    u_int32_t       sin6_flowinfo; // IPv6 flow information
-    struct in6_addr sin6_addr;     // IPv6 address
-    u_int32_t       sin6_scope_id; // Scope ID
-};
-
-struct in6_addr {
-    unsigned char   s6_addr[16];   // load with inet_pton()
-};
-*/
-
-
-// General socket address holding structure, big enough to hold either
-// struct sockaddr_in or struct sockaddr_in6 data:
-/**
- * struct sockaddr_storage {
-    sa_family_t  ss_family;     // address family
-
-    // all this is padding, implementation specific, ignore it:
-    char      __ss_pad1[_SS_PAD1SIZE];
-    int64_t   __ss_align;
-    char      __ss_pad2[_SS_PAD2SIZE];
-};
-*/
-
-//TEST: struct from configuration.hpp
-// namespace configuration
-// {
-// 	struct Socket
-// 	{
-// 		std::string ip;
-// 		std::string port;
-// 		unsigned int   family_;
-// 	};
-// }
-
-// struct SocketConfiguration
-// {
-// 	int server_socket;
-// };
 
 #define TIMEOUT 75
 
@@ -96,19 +26,6 @@ struct ClientSocket
 	Maybe<time_t> first_recv_time; //time of first recv of one request
 	bool timeout;
 };
-
-// {
-// 	Maybe<time_t> init_time;
-
-// 	if (init_time.is_ok())
-// 	{
-// 		init_time.value()
-// 	}
-// 	else
-// 	{
-// 		init_time = 130;
-// 	}
-// }
 
 //save the full linked list of res from getaddrinfo()
 //use addr_to_bind to query the correct node in the linked list
---
+++ src/socket_manager/socket_address.md
@@ -0,0 +1,67 @@
+### The structure of socket address
+#### The following structure is a linked list. It is used to prep the socket address structures for subsequent use. It’s also used in host name lookups, and service name lookups.
+```
+struct addrinfo {
+    int              ai_flags;     // AI_PASSIVE, AI_CANONNAME, etc.
+    int              ai_family;    // AF_INET, AF_INET6, AF_UNSPEC
+    int              ai_socktype;  // SOCK_STREAM, SOCK_DGRAM
+    int              ai_protocol;  // use 0 for "any"
+    size_t           ai_addrlen;   // size of ai_addr in bytes
+    struct sockaddr *ai_addr;      // struct sockaddr_in or _in6
+    char            *ai_canonname; // full canonical hostname
+
+    struct addrinfo *ai_next;      // linked list, next node
+};
+```
+- #### All pointers to socket address structures are often cast to pointers to the following type before use in various functions and system calls:
+```
+struct sockaddr {
+	unsigned short	sa_family;    // address family, AF_xxx
+    char			sa_data[14];  // 14 bytes of protocol address
+};
+```
+
+- - #### IPv4 AF_INET socket address:
+```
+struct sockaddr_in {
+    short            sin_family;   // e.g. AF_INET, AF_INET6
+    unsigned short   sin_port;     // e.g. htons(3490)
+    struct in_addr   sin_addr;     // see struct in_addr, below
+    char             sin_zero[8];  // zero this if you want to
+};
+
+struct in_addr {
+    unsigned long s_addr;          // load with inet_pton()
+};
+```
+
+- - #### IPv6 AF_INET6 sockets address:
+```
+struct sockaddr_in6 {
+    u_int16_t       sin6_family;   // address family, AF_INET6
+    u_int16_t       sin6_port;     // port number, Network Byte Order
+    u_int32_t       sin6_flowinfo; // IPv6 flow information
+    struct in6_addr sin6_addr;     // IPv6 address
+    u_int32_t       sin6_scope_id; // Scope ID
+};
+
+struct in6_addr {
+    unsigned char   s6_addr[16];   // load with inet_pton()
+};
+```
+- #### General socket address holding structure, big enough to hold either struct sockaddr_in or struct sockaddr_in6 data:
+```
+struct sockaddr_storage {
+    sa_family_t  ss_family;     // address family
+
+    // all this is padding, implementation specific, ignore it:
+    char      __ss_pad1[_SS_PAD1SIZE];
+    int64_t   __ss_align;
+    char      __ss_pad2[_SS_PAD2SIZE];
+};
+```
+
+### Reference for network programming:
+- https://beej.us/guide/bgnet/html/
+
+
