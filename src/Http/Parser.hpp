0c66411f1f9ee8bd2ef851daefc12b9c56564dfc
Anthony Tsang <anthonytsang0226@gmail.com>
2024-05-09 21:50:36 +0200

    feat(HttpParser): Create the ParseTree for HTTP message. Still need to create the struct for URI parsing

--- src/Http/Parser.hpp
+++ src/Http/Parser.hpp
@@ -1,17 +1,12 @@
 #pragma once
 
-#include <string>
 #include <vector>
+#include <string>
+
+#include "misc/Maybe.hpp"
 
 namespace http_parser
 {
-
-  struct StringSlice
-  {
-    const char* bytes;
-    int         length;
-  };
-
   /* enum CharType
   {
     kAlpha,
@@ -24,116 +19,215 @@ namespace http_parser
     kSp,
   }; */
 
-  /* token                = 1*tchar
-  tchar                =
-   "!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "." /
-   "^" / "_" / "`" / "|" / "~" / DIGIT / ALPHA
-
-  ; Quoted Strings (RFC9110 5.6.4)
-  quoted-string        = DQUOTE *( qdtext / quoted-pair ) DQUOTE
-  qdtext               =
-   HTAB / SP / "!" / %x23-5B ; '#'-'['
-   / %x5D-7E ; ']'-'~'
-   / obs-text
-  quoted-pair          = "\" ( HTAB / SP / VCHAR / obs-text )
-
-  ; comment (RFC9110 5.6.5)
-  comment              = "(" *( ctext / quoted-pair / comment ) ")"
-  ctext                =
-   HTAB / SP / %x21-27 ; '!'-'''
-   / %x2A-5B ; '*'-'['
-   / %x5D-7E ; ']'-'~'
-   / obs-text
-  
-  ; Parameters a.k.a key and value pairs delimited by '=' and delimited by ';' (RFC9110 5.6.6) 
-  parameters           = *( OWS ";" OWS [ parameter ] )
-  parameter            = parameter-name "=" parameter-value
-  parameter-name       = token
-  parameter-value      = ( token / quoted-string ) */
-
-  enum TokenType
+  struct StringSlice
   {
-    kWord,
-    kQuotedWord,
-    kDquote,
-    kLBracket,
-    kRBracket
-
-  };
-
-  struct Token
-  {
-    TokenType   type;
-    StringSlice string;
-  };
-
-  class Tokenizer
-  {
-    public:
-      Tokenizer();
-      Tokenizer(const std::string& string);
-
-      void  set_string(const std::string& string);
-      Token tokenize();
-      void  untokenize(Token token);
-
-    private:
-      const char*         start;
-      const char*         end;
-      const char*         current;
-      std::vector<Token>  untokenized; // used as a stack of tokens
+    const char* bytes;
+    int         length;
   };
 
   typedef const char* Input;
-  struct Output
+  struct ScanOutput
   {
-    int     length;
-    Input   rest;
-
-    bool    is_success();
+    bool        is_success;
+    StringSlice string;
+    Input       rest;
   };
 
-  Output  ParseAlpha(Input input);
-  Output  ParseBit(Input input);
-  Output  ParseChar(Input input);
-  Output  ParseCarriageReturn(Input input);
-  Output  ParseControls(Input input);
-  Output  ParseDigit(Input input);
-  Output  ParseDoubleQuote(Input input);
-  Output  ParseHexDigit(Input input);
-  Output  ParseHorizontalTab(Input input);
-  Output  ParseLinefeed(Input input);
-  Output  ParseLinearWhiteSpace(Input input);
-  Output  ParseOctet(Input input);
-  Output  ParseSpace(Input input);
-  Output  ParseVisibleCharacter(Input input);
-  Output  ParseWhitespace(Input input);
+  ScanOutput  ScanAlpha(Input input);
+  ScanOutput  ScanBit(Input input);
+  ScanOutput  ScanChar(Input input);
+  ScanOutput  ScanCarriageReturn(Input input);
+  ScanOutput  ScanControls(Input input);
+  ScanOutput  ScanDigit(Input input);
+  ScanOutput  ScanDoubleQuote(Input input);
+  ScanOutput  ScanHexDigit(Input input);
+  ScanOutput  ScanHorizontalTab(Input input);
+  ScanOutput  ScanLinefeed(Input input);
+  ScanOutput  ScanLinearWhiteSpace(Input input);
+  ScanOutput  ScanOctet(Input input);
+  ScanOutput  ScanSpace(Input input);
+  ScanOutput  ScanVisibleCharacter(Input input);
+  ScanOutput  ScanWhitespace(Input input);
 
-  Output  ParseOptionalWhitespace(Input input);
-  Output  ParseRequiredWhitespace(Input input);
-  Output  ParseBadWhitespace(Input input);
+  ScanOutput  ScanOptionalWhitespace(Input input);
+  ScanOutput  ScanRequiredWhitespace(Input input);
+  ScanOutput  ScanBadWhitespace(Input input);
 
   ///////////////////////////////////////////
   ////////////////   text    ////////////////
   ///////////////////////////////////////////
 
-  Output  ParseQuotedStringText(Input input);
-  Output  ParseCommentText(Input input);
-  Output  ParseOpaqueText(Input input); // named as obs-text in abnf
-  Output  ParseEscapedText(Input input); // named as quoted-pair in abnf
+  ScanOutput  ScanQuotedStringText(Input input);
+  ScanOutput  ScanCommentText(Input input);
+  ScanOutput  ScanOpaqueText(Input input); // named as obs-text in abnf
+  ScanOutput  ScanEscapedText(Input input); // named as quoted-pair in abnf
 
   ///////////////////////////////////////////
   ////////////////   token   ////////////////
   ///////////////////////////////////////////
 
-  typedef Output  AToken;
+  enum ParseNodeType
+  {
+    kToken,
+    kQuotedString,
+    kComment,
+    kParameter,
+    kParameters,
 
-  AToken   ParseToken(Input input);
-  Output  ParseTokenCharacter(Input input);
+    kHttpVersion,
 
-  Output  ParseQuotedString(Input input);
-  Output  ParseComment(Input input);
+    kRequestLine,
+    kRequestTargetOriginForm,
+    kRequestTargetAbsoluteForm,
+    kRequestTargetAuthorityForm,
+    kRequestAsteriskForm,
 
-  Output  ParseHttpMessage(Input input); 
+    kStatusLine,
+    kStatusCode,
+    kStatusReasonPhrase,
+
+    kFieldLine,
+    kMessageBody,
+    kMessage
+  };
+
+  struct ParseNode
+  {
+    ParseNodeType type;
+  };
+
+  struct ParseNodeToken : public ParseNode
+  {
+    std::string content;
+  };
+
+  struct ParseNodeQuotedString : public ParseNode
+  {
+    std::string content;
+  };
+
+  struct ParseNodeComment : public ParseNode
+  {}; // ignore comment content
+
+  struct ParseNodeParameter : public ParseNode
+  {
+    ParseNodeToken* name;
+    ParseNodeType   value_type;
+    union
+    {
+      ParseNodeToken*         value;
+      ParseNodeQuotedString*  value_quoted;
+    };
+  };
+
+  struct ParseNodeParameters : public ParseNode
+  {
+    std::vector<ParseNodeParameter*> children;
+  };
+
+  struct ParseNodeHttpVersion : public ParseNode
+  {
+    std::string content;
+  };
+
+  //////////////////////////////////////////////////
+  ////////////////   Request line   ////////////////
+  //////////////////////////////////////////////////
+
+  struct ParseNodeRequestTargetOriginForm : public ParseNode 
+  {
+    // TODO: need to parse URI stuffs
+  };
+
+  struct  ParseNodeRequestTargetAbsoluteForm : public ParseNode
+  {
+    // TODO: need to parse URI stuffs
+  };
+
+  struct ParseNodeRequestTargetAuthorityForm : public ParseNode
+  {
+    // TODO: need to parse URI stuffs
+  };
+
+  struct ParseNodeRequestTargetAsteriskForm : public ParseNode
+  {};
+  
+  struct ParseNodeRequestLine : public ParseNode
+  {
+    ParseNodeToken* method;
+    ParseNodeType   request_target_type;
+    union
+    {
+      ParseNodeRequestTargetOriginForm*     request_target_origin_form;
+      ParseNodeRequestTargetAbsoluteForm*   request_target_absolute_form;
+      ParseNodeRequestTargetAuthorityForm*  request_target_authority_form;
+      ParseNodeRequestTargetAsteriskForm*   request_target_asterisk_form;
+    };
+    ParseNodeHttpVersion* version;
+  };
+
+  /////////////////////////////////////////////////
+  ////////////////   status line   ////////////////
+  /////////////////////////////////////////////////
+
+  struct ParseNodeReasonPhrase : public ParseNode
+  {
+    std::string content;
+  };
+
+  struct ParseNodeStatusLine : public ParseNode
+  {
+    ParseNodeHttpVersion*         http_version;
+    int                           status_code;
+    Maybe<ParseNodeReasonPhrase*> reason_phrase;
+  };
+
+  struct ParseNodeFieldValue : public ParseNode
+  {
+    std::string content;
+  };
+
+  struct ParseNodeFieldLine : public ParseNode
+  {
+    ParseNodeToken*       name;
+    ParseNodeFieldValue*  value;
+  };
+
+  struct ParseNodeMessageBody : public ParseNode
+  {
+    std::string content;
+  };
+
+  struct ParseNodeMessage : public ParseNode
+  {
+    ParseNodeType start_line_type;
+    union
+    {
+      ParseNodeRequestLine* request_line;
+      ParseNodeStatusLine*  status_line;
+    };
+    std::vector<ParseNodeFieldLine*>  fields;
+    Maybe<ParseNodeMessageBody*>      body;
+  };
+
+  struct ParseTree
+  {
+    ParseNode*  root;
+    ParseNode*  current;
+  };
+
+  ScanOutput  ParseToken(Input input);
+  ScanOutput  ScanTokenCharacter(Input input);
+
+  ScanOutput  ParseQuotedString(Input input);
+  ScanOutput  ScanQuotedStringCharacter(Input input);
+
+  ScanOutput  ParseComment(Input input);
+  ScanOutput  ScanCommentCharacter(Input input);
+
+  ScanOutput  ParseParameters(Input input);
+  ScanOutput  ParseParameter(Input input);
+
+  ScanOutput  ParseHttpMessage(Input input, ParseTree* tree);
 } // namespace http_parser
 
