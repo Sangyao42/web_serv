0ead8eb99660d6dcd4fa1ba58ae3f54a186cef27
Sangyao42 <wangsangyao@hotmail.com>
2024-05-10 21:02:19 +0200

    feat(cgi): Modified process cgi GET request

--- ClassDesignTmp/Cgi.cpp
+++ ClassDesignTmp/Cgi.cpp
@@ -1,80 +1,85 @@
 #include "Client.hpp"
 
+#include <sys/wait.h>
 #include <cassert>
 
-void	process::ProcessGetRequestCgi(struct Client *clt);
+void	process::ProcessGetRequestCgi(struct Client *clt)
 {
 	int	cgi_input[2], cgi_output[2];
-	int pipes = SetPipes(cgi_input[2], cgi_output[2], clt->req->getMethods()); //For CGI GET request, only cgi_output[2] is needed
+	int pipes = SetPipes(cgi_input, cgi_output, clt->req->getMethod()); //For CGI GET request, only cgi_output[2] is needed
 	if (pipes < 0)
 	{
-		clt->statusCode = k500;
-		return (generateErrorResponse(clt));
+		clt->status_code = k500;
+		return (resBuilder::GenerateErrorResponse(clt));
 	}
 	int pid = fork();
 	if (pid < 0)
 	{
 		close(cgi_output[PipeEnd::kRead]);
 		close(cgi_output[PipeEnd::kWrite]);
-		clt->statusCode = k500;
-		return (generateErrorResponse(clt));
+		clt->status_code = k500;
+		return (resBuilder::GenerateErrorResponse(clt));
 	}
 	if (pid == 0)
 	{
 		//child process
 		close(cgi_output[PipeEnd::kRead]);
-		dup2(cgi_output[PipeEnd::kWrite], STDOUT);
-		CheckCgiFileExtension(); // get which cgi it is
-		char* cgi_path = ConstructCgiExcutable(); // get excutable from location->cgis based on the file extension and check permission
-		if (!executable_file_is_X_OK || !cgi_is_R_OK)
+		dup2(cgi_output[PipeEnd::kWrite], STDOUT_FILENO);
+		char* cgi_executable = ConstructCgiExcutable(); // get excutable from looping through location->cgis based on the file extension and check permission
+		char* cgi_path = ConstructCgiPath(); // get excutable from looping through location->cgis based on the file extension and check permission
+		if (access(cgi_executable, X_OK) != 0 || access(cgi_path, R_OK) != 0)
 		{
-			close(cgi_output[WRITE]);
-			clt->statusCode = k500;
-			return (generateErrorResponse(clt));
+			close(cgi_output[PipeEnd::kWrite]);
+			exit(1);
 		}
-		char** cgi_argv = ConstructCgiArgv();
+		char** cgi_argv = ConstructCgiArgv(cgi_path);
 		char** cgi_env = ConstructCgiEnv();
-		execve(cgi_path, cgi_argv, cgi_env);
-		// TODO: if execve failed, clean cgi_argv and cgi_env, and close the pipe
-		close(cgi_output[WRITE]);
+		if (cgi_argv == NULL || cgi_env == NULL)
+		{
+			close(cgi_output[PipeEnd::kWrite]);
+			exit(1);
+		}
+		execve(cgi_executable, cgi_argv, cgi_env);
+		int checkfree = FreeTwoDimArray(cgi_argv);
+		int checkfree2 = FreeTwoDimArray(cgi_env);
+		close(cgi_output[PipeEnd::kWrite]);
+		exit(1);
 	}
 	//parent process
 	std::string response_tmp;
-	close(cgi_output[WRITE]);
+	close(cgi_output[PipeEnd::kWrite]);
 	int wstats;
 	waitpid(pid, &wstats, 0);
-	if (WIFEXITED(wtsats) && WEXITSTATUS(wstats) == 0)
+	if (WIFEXITED(wstats) && WEXITSTATUS(wstats) == 0)
 	{
-		int read = ReadAll(cgi_output[READ], response_tmp);
-		close(cgi_output[READ]);
+		int read = process::ReadAll(cgi_output[PipeEnd::kRead], response_tmp);
+		close(cgi_output[PipeEnd::kRead]);
 		if (read < 0)
 		{
-			clt->statusCode = k500;
-			return (generateErrorResponse(clt));
+			clt->status_code = k500;
+			return (resBuilder::GenerateErrorResponse(clt));
 		}
-		ParseResponseTmp(); // modify the clt->response by the received message from cgi
-		//generate responses based on the status code read from the cgi output
-		if (clt->statusCode && clt->statusCode >= k400)
-			return (generateErrorResponse(clt));
-		if (content-type != clt->req->returnValueAsPointer("Accept"))
+		ParseResponseTmp(response_tmp); // modify the clt->response by the received message from cgi
+		if (IsAccessable("content-type")) // TODO: get the content type from the string returned by cgi
 		{
-			clt->statusCode = k406;
-			return (generateErrorResponse(clt));
+			clt->status_code = k406;
+			return (resBuilder::GenerateErrorResponse(clt));
 		}
 		else
 		{
-			clt->statusCode = k200;
-			return (generateSuccessResponse(clt))
+			clt->status_code = k200;
+			return (resBuilder::GenerateSuccessResponse(clt));
 		}
 	}
 	else
 	{
-		clt->statusCode = k500;
-		return (generateErrorResponse(clt));
+		clt->status_code = k500;
+		return (resBuilder::GenerateErrorResponse(clt));
 	}
 }
 
-int SetPipes(int *cgi_input, int *cgi_output, const std::string &method)
+//cgi related functions
+int process::SetPipes(int *cgi_input, int *cgi_output, const Method method)
 {
 	assert((method == kGet || method == kPost) && "SetPipes: method is not GET or POST");
 	if (pipe(cgi_output) < 0)
@@ -88,4 +93,26 @@ int SetPipes(int *cgi_input, int *cgi_output, const std::string &method)
 			return (-1);
 		}
 	}
+}
+
+char*	process::ConstructCgiPath()
+{
+}
+
+char*	process::ConstructCgiExcutable()
+{
+	// get excutable from looping through location->cgis based on the file extension and check permission
+}
+
+char**	process::ConstructCgiArgv(char* cgi_path)
+{
+}
+
+char**	process::ConstructCgiEnv()
+{
+}
+
+//helper functions
+int	process::FreeTwoDimArray(char **argv)
+{
 }
