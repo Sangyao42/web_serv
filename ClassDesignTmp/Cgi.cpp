09889d41bfc3ba0a17cbe79dcb40ee6a91f2f751
Sangyao42 <wangsangyao@hotmail.com>
2024-05-14 20:59:39 +0200

    feat(cgi): Add set env for execve(), leave server-related env undone.

--- ClassDesignTmp/Cgi.cpp
+++ ClassDesignTmp/Cgi.cpp
@@ -25,7 +25,7 @@ void	process::ProcessGetRequestCgi(struct Client *clt)
 		//child process
 		close(cgi_output[PipeEnd::kRead]);
 		dup2(cgi_output[PipeEnd::kWrite], STDOUT_FILENO);
-		assert(!clt->cgi_argv.empty() && "ProcessGetRequestCgi: clt->cgi_executable is NULL");
+		assert(!clt->cgi_argv.empty() && "ProcessGetRequestCgi: clt->cgi_argv is NULL");
 		// char* cgi_executable = clt->cgi_executable.c_str(); // get excutable from looping through location->cgis based on the file extension and check permission
 		// char* cgi_path = clt->path.c_str(); // get excutable from looping through location->cgis based on the file extension and check permission
 		if (access(clt->cgi_argv[0].c_str(), F_OK | X_OK) != 0 || \
@@ -64,7 +64,7 @@ void	process::ProcessGetRequestCgi(struct Client *clt)
 			return (res_builder::GenerateErrorResponse(clt));
 		}
 		ParseResponseTmp(response_tmp); // modify the clt->response by the received message from cgi
-		std::string res_content_type = response_tmp.substr(response_tmp.find("Content-Type: "),response_tmp.find("\r\n\r\n")); // TODO: get the content type from the string returned by cgi
+		std::string res_content_type = response_tmp.substr(response_tmp.find("Content-Type: "),response_tmp.find("\r\n\r\n")); // TODO: get the content type from the string returned by cgi, in std::string or a vector of string
 		if (!IsSupportedMediaType(res_content_type, clt->config->query->mime_types)) //check the response content type with the MIME type
 		{
 			clt->status_code = k500;
@@ -209,17 +209,7 @@ int process::SetPipes(int *cgi_input, int *cgi_output, const Method method)
 	}
 }
 
-std::vector<char *>	process::ConstructExecArray(std::vector<std::string> &cgi_params) //{extension, cgi_path, NULL}
-{
-	std::vector<char*> cstrings;
-	int cstrs_size = StringVecToTwoDimArray(cstrings, cgi_params);
-	if (cstrs_size == 1)
-	{
-		std::cerr << "Error: ConstructCgiArgv" << std::endl;
-		return (std::vector<char*>());
-	}
-	return cstrings;
-}
+#define stringify(name) #name
 
 void	process::SetCgiEnv(struct Client *clt)
 {
@@ -227,12 +217,63 @@ void	process::SetCgiEnv(struct Client *clt)
 	//request_uri, document_uri, document_root, script_name,
 	//server_addr, server_port, server_protocol, server_name
 
+	//construct method
+	// char* convert_enum_to_string[] = {stringify(Monday), stringify(Tuesday), stringify(Wednesday), stringify(Thursday)};
+	// std::string method = convert_enum_to_string[clt->req->getMethod()];
+	// clt->cgi_env.push_back("REQUEST_METHOD=" + method);
+	clt->cgi_env.push_back("REQUEST_METHOD=" + clt->req->getMethod());
+
+	//construct query_string
+	uri::Query query = clt->req->getRequestTarget().query;
+	std::string query_string;
+	for (size_t i = 0; i < query.size(); ++i)
+	{
+		if (i == query.size() - 1)
+			query_string += query[i].first + "=" + query[i].second;
+		else
+			query_string += query[i].first + "=" + query[i].second + "&";
+	}
+	clt->cgi_env.push_back("QUERY_STRING=" + query_string);
+
+	//construct content_length
+	HeaderInt *content_length = dynamic_cast<HeaderInt *>(clt->req->returnValueAsPointer("Content-Length"));
+	std::ostringstream content_length_str;
+	content_length->toStringStream(content_length_str);
+	if (content_length && content_length_str.good())
+		clt->cgi_env.push_back("CONTENT_LENGTH=" + content_length_str.str());
+	else
+		clt->cgi_env.push_back("CONTENT_LENGTH=");
+
+	//construct content_type //TODO: get content type from the request header as HeaderString or vector of string
+	HeaderString *content_type = dynamic_cast<HeaderString *>(clt->req->returnValueAsPointer("Content-Type"));
+	if (content_type)
+		clt->cgi_env.push_back("CONTENT_TYPE=" + content_type->content());
+	else
+		clt->cgi_env.push_back("CONTENT_TYPE=");
+
+	//construct request_uri
+	clt->cgi_env.push_back("REQUEST_URI=" + clt->req->getRequestTarget().path);
+
+	//construct document_uri // ? what is this? Probably for POST request with Location header ?
+
+	//construct document_root
+	std::string document_root = clt->config->query->root;
+	clt->cgi_env.push_back("DOCUMENT_ROOT=" + document_root); //? without the prefix . ?
+
+	//construct script_name
+	size_t pos = clt->path.find(clt->config->query->root) + clt->config->query->root.size();
+	std::string script_name = clt->path.substr(pos);
+	clt->cgi_env.push_back("SCRIPT_NAME=" + script_name);
+
+	//construct scipt_filename
+	clt->cgi_env.push_back("SCRIPT_FILENAME=" + document_root + script_name);
+
 }
 // fastcgi_param  SCRIPT_FILENAME    $document_root$fastcgi_script_name;
 // fastcgi_param  QUERY_STRING       $query_string;
 // fastcgi_param  REQUEST_METHOD     $request_method;
 // fastcgi_param  CONTENT_TYPE       $content_type;
-// fastcgi_param  CONTENT_LENGTH     $content_length;
+// fastcgi_param  CONTENT_LENGTH     $content_length;d
 // fastcgi_param  SCRIPT_NAME        $fastcgi_script_name;
 // fastcgi_param  REQUEST_URI        $request_uri;
 // fastcgi_param  DOCUMENT_URI       $document_uri;
@@ -249,6 +290,18 @@ void	process::SetCgiEnv(struct Client *clt)
 // fastcgi_index  index.php;
 
 // fastcgi_param  REDIRECT_STATUS    200;
+
+std::vector<char *>	process::ConstructExecArray(std::vector<std::string> &cgi_params) //{extension, cgi_path, NULL}
+{
+	std::vector<char*> cstrings;
+	int cstrs_size = StringVecToTwoDimArray(cstrings, cgi_params);
+	if (cstrs_size == 1)
+	{
+		std::cerr << "Error: ConstructCgiArgv" << std::endl;
+		return (std::vector<char*>());
+	}
+	return cstrings;
+}
 
 //helper functions
 int	process::StringVecToTwoDimArray(std::vector<char *> &cstrings,const std::vector<std::string> &strings)
