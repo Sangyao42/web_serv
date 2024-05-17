41badcc76b0c8030addd119bf76ce7ce27daf9c3
Sangyao42 <106156781+Sangyao42@users.noreply.github.com>
2024-05-18 01:28:55 +0200

    fix(scripts): fetch error in js

--- www/list.py
+++ www/list.py
@@ -12,37 +12,36 @@ def index():
     for file in files:
         if file != '.' and file != '..':
             file_path = os.path.join(directory, file)
-            html += f"<ul><li><a href='\{file_path}' id='{file}'>{file}</a></li>"
+            html += f"<ul><li><a href='\{file_path}' id='filedel'>{file}</a></li>"
             html += "<input type='button' value='delete' id='delete'></ul>"
-    # script_file = open("del.js","r")
-    # script = script_file.read()
-    # script_clean = script.strip()
-    html += "</body>"
+    script_file = open("del.js", "r")
+    script = script_file.read()
+    html += "</body><script>" + script + "</script></html>"
 
-    for file in files:
-        if file != '.' and file != '..':
-            script_clean = " \
-        function main()\
-        {const fileToDelete = " + f"document.getElementById('{file}')" +";\
-		const fileLocation = fileToDelete.href; \
-        const deleteBtn = document.querySelector('input[type=\"button\"]');\
-            deleteBtn.addEventListener('click', () => { \
-                fetch(fileLocation, { \
-                    method: 'DELETE', \
-                }) \
-            .then(response => response.text()) \
-            .then(data => { \
-                console.log(data); \
-            }) \
-            .catch(error => { \
-                console.error(error); \
-            }); \
-        }); \
-    } \
-    document.addEventListener('DOMContentLoaded', main); \
-    "
-            html += "<script>" + script_clean
-            html +=  "</script>"
+    # for file in files:
+    #     if file != '.' and file != '..':
+    #         script_clean = " \
+    #     function main()\
+    #     {const fileToDelete = " + f"document.getElementById('{file}')" +";\
+	# 	const fileLocation = fileToDelete.href; \
+    #     const deleteBtn = document.querySelector('input[type=\"button\"]');\
+    #         deleteBtn.addEventListener('click', () => { \
+    #             fetch(fileLocation, { \
+    #                 method: 'DELETE', \
+    #             }) \
+    #         .then(response => response.text()) \
+    #         .then(data => { \
+    #             console.log(data); \
+    #         }) \
+    #         .catch(error => { \
+    #             console.error(error); \
+    #         }); \
+    #     }); \
+    # } \
+    # document.addEventListener('DOMContentLoaded', main); \
+    # "
+    #         html += "<script>" + script_clean
+    #         html +=  "</script>"
     html += "</html>"
 
     f = open("www/delete1.html", "w")
