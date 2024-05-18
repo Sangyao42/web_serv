e06cb191bfe555f729a67f13552c02f4dc6e796b
Eunsol Kim <eunskim@student.42heilbronn.de>
2024-05-18 11:28:16 +0200

    feat(HeaderValue): added a draft of HeaderStringVector class

---
+++ ClassDesignTmp/HeaderValue/HeaderStringVector.cpp
@@ -0,0 +1,73 @@
+#include "HeaderStringVector.hpp"
+
+HeaderStringVector::HeaderStringVector()
+: type_(kStringVector), content_(StringVector()) {}
+
+HeaderStringVector::HeaderStringVector(const StringVector &content)
+: type_(kStringVector), content_(content) {}
+
+HeaderStringVector::HeaderStringVector(const HeaderStringVector &obj)
+: type_(obj.type_), content_(obj.content_) {}
+
+HeaderStringVector::~HeaderStringVector()
+{
+	content_.clear();
+}
+
+HeaderStringVector &HeaderStringVector::operator=(const HeaderStringVector &obj)
+{
+	content_.clear();
+	type_ = obj.type_;
+	content_ = obj.content_;
+}
+
+const HeaderStringVector::ValueType	&HeaderStringVector::type() const
+{
+	return (type_);
+}
+
+const StringVector	&HeaderStringVector::content() const
+{
+	return (content_);
+}
+
+HeaderStringVector	*HeaderStringVector::clone() const
+{
+	return (new HeaderStringVector(*this));
+}
+
+void	HeaderStringVector::toStringStream(std::ostringstream &oss)
+{
+	for (StringVectorIt it = content_.begin(); it != content_.end(); ++it)
+	{
+		oss << *it;
+		if (it + 1 != content_.end())
+			oss << ", ";
+	}
+}
+
+void	HeaderStringVector::addString(const std::string &str)
+{
+	content_.push_back(str);
+}
+
+void	HeaderStringVector::addString(const char *str)
+{
+	content_.push_back(std::string(str));
+}
+
+void	HeaderStringVector::addString(const char *str, size_t len)
+{
+	content_.push_back(std::string(str, len));
+}
+
+size_t	HeaderStringVector::size() const
+{
+	return (content_.size());
+}
+
+const std::string	&HeaderStringVector::operator[](size_t index) const
+{
+	assert(index < content_.size() && "Index out of bounds");
+	return (content_[index]);
+}
