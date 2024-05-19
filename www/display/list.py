93e40d29e541576d2002eed0b5bfa014be7be657
Sangyao42 <wangsangyao@hotmail.com>
2024-05-19 20:19:22 +0200

    feat(scripts): Modify the .py to work at root

--- www/display/list.py
+++ www/display/list.py
@@ -2,29 +2,34 @@ import os
 import sys
 
 def index():
-    dir_cgi = os.path.dirname(__file__)
-    dir_parent = os.path.dirname(dir_cgi)
-    upload_folder = os.path.join( dir_parent, 'upload')
+	# TODO: for getting the list of file
+    # root = os.environ['DOCUMENT_ROOT']
+    root = './www'
+    upload_folder = root + '/upload'
     files = os.listdir(upload_folder)
 
+	# for generating uri
     dir_path = '/upload'
 
 	# load script for list all the files
+    dir_cgi = os.path.dirname(__file__)
     script_file = open( os.path.join(dir_cgi, 'del.js'), "r")
     script = script_file.read()
 
     html = "<!DOCTYPE html>\n<html><body><h1>Directory Listing</h1>"
     for file in files:
         if file != '.' and file != '..':
-            file_path = os.path.join('..', dir_path, file)
+            file_path = os.path.join(dir_path, file)
             html += f"<ul><li><a href='{file_path}'>{file}</a></li>"
             html += f"<input type='button' value='delete' id='delete' onclick='del_file(\"{file_path}\")'></ul>"
     html += "</body>\n"
     html += "<script>" + script + "</script></html>"
 
+	# For testing generated html
     f = open( os.path.join(dir_cgi, 'delete1.html'), "w+")
     f.write(html)
     f.close()
+    
     # TODO: Add "Content-Type in html"
     print(html)
 
--- www/register/save_username.py
+++ www/register/save_username.py
@@ -1,31 +1,23 @@
 import json
 import sys
+import os
 from sys import stdin
 
-# body = input()
-# user_name_json_obj = json.loads(body)
-
-# user_name = user_name_json_obj["userName"]
-
-# if not user_name:
-#     exit (1)
-
-# f = open('usernames/usernames.txt', 'a')
-# f.write(user_name + '\n')
-# f.close()
-# print("Content-Type=text/html\r\n\r\n")
-# print("Registration Success!")
-
 def save_registry():
+    # TODO: get document root from parent process
+    # root = os.environ['DOCUMENT_ROOT']
+    root = './www'
     # read from stdin. TODO: need test with execve
-    body = sys.stdin.read()
+    # TODO: for testing stdin
+    body = input()
+    # body = sys.stdin.read()
     user_name_json_obj = json.loads(body)
     user_name = user_name_json_obj["userName"]
     if not user_name:
         sys.exit(1)
 
 	# write to file
-    f = open('usernames/usernames.txt', 'a+')
+    f = open(root + '/usernames/usernames.txt', 'a+')
     f.write(user_name + '\n')
     f.close()
     print("Content-Type=text/plain\r\n\r\n")
