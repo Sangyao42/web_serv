355e4faf3c2697dc3d55e238cefde8129f77dc7c
Sangyao <wangsangyao@hotmail.com>
2024-06-08 17:16:23 +0200

    refactor(process): Move header-related only checkings into logic in main

--- ClassDesignTmp/Process.cpp
+++ ClassDesignTmp/Process.cpp
@@ -6,11 +6,12 @@
 
 void	process::ProcessRequest(struct Client *clt)
 {
-	if (!clt || !clt->client_socket || !clt->req || !clt->res)
-	{
-		clt->status_code = k500;
-		return (res_builder::GenerateErrorResponse(clt));
-	}
+	// if (!clt || !clt->client_socket || !clt->req || !clt->res)
+	// {
+	// 	clt->status_code = k500;
+	// 	return (res_builder::GenerateErrorResponse(clt));
+	// }
+	assert(clt && clt->client_socket && clt->req && clt->res && "Client or client_socket or req or res is null");
 
 	if (clt->status_code != k000)
 		return (res_builder::GenerateErrorResponse(clt)); // there is an existing error
@@ -19,54 +20,9 @@ void	process::ProcessRequest(struct Client *clt)
 	if (connection && connection->content() == "close")
 		clt->keepAlive = false;
 
-	HeaderString	*Host = dynamic_cast<HeaderString *> (clt->req->returnValueAsPointer("Host"));
-
-	// query configuration
-	clt->config = &ws_database.query(clt->client_socket->socket, \
-		(Host ? Host->content() : ""), \
-		clt->req->getRequestTarget().path);
-
-	if (clt->config->is_empty())
-	{
-		clt->status_code = k500;
-		return (res_builder::GenerateErrorResponse(clt));
-	}
-
-	assert(clt->config->query && "Location Block in Server block is empty"); //query gives the all the needed info related to server block and location block
-	cache::LocationQuery	*location= clt->config->query;
-	if (!(location->allowed_methods & (int) clt->req->getMethod()))
-	{
-		clt->status_code = k405;
-		return (res_builder::GenerateErrorResponse(clt));
-	}
-	if (location->redirect)
+	//check redirect
+	if (clt->config->query->redirect)
 		return (res_builder::GenerateRedirectResponse(clt));
-
-	std::string path = process::GetExactPath(location->root, location->match_path, clt->req->getRequestTarget());
-	clt->path = path;
-	if ((access(path.c_str(), F_OK) != 0) \
-	&& (clt->req->getMethod() == kGet || clt->req->getMethod() == kDelete)) // check if the path exists (for get and delete)
-	{
-		clt->status_code = k404;
-		return (res_builder::GenerateErrorResponse(clt));
-	}
-	else
-	{
-		if (stat(path.c_str(), &clt->stat_buff) != 0)
-		{
-			clt->status_code = k500;
-			return (res_builder::GenerateErrorResponse(clt));
-		}
-	}
-
-	HeaderInt *content_length = dynamic_cast<HeaderInt *> (clt->req->returnValueAsPointer("Content-Length"));
-
-	if ((content_length && content_length->content() > location->client_max_body_size) || \
-	(clt->req->body_size_chunked_ != -1 && clt->req->body_size_chunked_ > location->client_max_body_size))
-	{
-		clt->status_code = k413;
-		return (res_builder::GenerateErrorResponse(clt));
-	}
 
 	switch(clt->req->getMethod())
 	{
