75fda3f63c6245ee662ce9717f760eceb8afabfa
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2024-06-29 18:04:28 +0200

    fix(HttpParser): clean arena after using it

--- src/main.cpp
+++ src/main.cpp
@@ -207,10 +207,10 @@ int main(int argc, char **argv)
 					//parse request
 					if (!clt->continue_reading)
 					{
-            			size_t  find_index = clt->client_socket->req_buf.find("\r\n");
+            size_t  find_index = clt->client_socket->req_buf.find("\r\n");
 						if (find_index != std::string::npos)
 						{
-              				http_parser::ParseOutput  parsed_request_line = http_parser::ParseRequestLine(http_parser::StringSlice(clt->client_socket->req_buf.c_str(), find_index + 2));
+              http_parser::ParseOutput  parsed_request_line = http_parser::ParseRequestLine(http_parser::StringSlice(clt->client_socket->req_buf.c_str(), find_index + 2));
 							if (!parsed_request_line.is_valid())
 							{
 								//handle only syntax error
@@ -218,6 +218,7 @@ int main(int argc, char **argv)
 								clt->keepAlive = false;
 								process::ProcessRequest(clt);
 								pfds[i].events = POLLOUT;
+                temporary::arena.clear();
 								continue;
 							}
 							else
@@ -230,11 +231,12 @@ int main(int argc, char **argv)
 								clt->req.setRequestLine(request_line);
 								clt->client_socket->req_buf.erase(0, parsed_request_line.length + 2);
 							}
+              temporary::arena.clear();
 						}
             			find_index = clt->client_socket->req_buf.find("\r\n\r\n");
 						if (find_index != std::string::npos)
 						{
-              				http_parser::ParseOutput parsed_headers = ParseFields(http_parser::StringSlice(clt->client_socket->req_buf.c_str(), find_index + 4));
+              http_parser::ParseOutput parsed_headers = ParseFields(http_parser::StringSlice(clt->client_socket->req_buf.c_str(), find_index + 4));
 							if (!parsed_headers.is_valid())
 							{
 								//handle only syntax error
@@ -242,6 +244,7 @@ int main(int argc, char **argv)
 								clt->keepAlive = false;
 								process::ProcessRequest(clt);
 								pfds[i].events = POLLOUT;
+                temporary::arena.clear();
 								continue;
 							}
 							else
@@ -252,6 +255,7 @@ int main(int argc, char **argv)
                 clt->status_code = ParseErrorToStatusCode(error);
 								clt->client_socket->req_buf.erase(0, parsed_headers.length + 2);
 							}
+              temporary::arena.clear();
 						}
 						client_lifespan::CheckHeaderBeforeProcess(clt);
 					}
